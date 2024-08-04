19f13441add683acfb945e210bdc1676f99faa89
Ali Habibi <ali.habibi281@gmail.com>
2024-08-04 23:26:50 +0330

    some changes

--- src/module/reserve/reserve.module.ts
+++ src/module/reserve/reserve.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReserveService } from './reserve.service';
 import { ReserveController } from './reserve.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReserveEntity } from './reserve.entity';
+import { AllDaysPossibleEntity } from '../all-days-possible/all-days-possible.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReserveEntity, AllDaysPossibleEntity])],
   providers: [ReserveService],
-  controllers: [ReserveController]
+  controllers: [ReserveController],
+  exports: [ReserveService],
 })
 export class ReserveModule {}
